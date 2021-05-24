43d442941b674971b408073ff6a8e78dfe91d20b
Sergio Martins <sergiomos2010@gmail.com>
2021-05-24 00:13:50 -0300

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/Header.css'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Countdown</h1>
+      <h2> Sérgio Martins -  Turma 10 A</h2>
+    </header>
+  )
+}
