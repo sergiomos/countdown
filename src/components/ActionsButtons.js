70def11951f0a3d13b665ee894c982c22d1bcbda
Sergio Martins <sergiomos2010@gmail.com>
2021-06-03 10:36:12 -0300

    Fix: Remove play utton when is code time

--- src/components/ActionsButtons.js
+++ src/components/ActionsButtons.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react';
 import { BsFillPauseFill, BsFillPlayFill, BsFillSquareFill } from 'react-icons/bs'
 
 export default class ActionsButtons extends Component {
+  renderPlayPouseButton = (status, handleClick) => (
+    <button 
+      onClick={ handleClick }
+      className="playPause"
+    >
+      { status !== 'started' ? <BsFillPlayFill /> : <BsFillPauseFill />}
+      
+    </button>
+  )
+
+  renderStopButton = (onClickStop) => (
+    <button 
+    onClick={ () => onClickStop('stopped') }
+    className="stop"
+    >
+      <BsFillSquareFill/>
+    </button>
+  )
+
   render() {
     const { onClick: handleClick, status, onClickStop } = this.props;
     return (
       <div className="actionsButtons">
-          <button 
-            onClick={ handleClick }
-            className="playPause"
-          >
-            { status !== 'started' ? <BsFillPlayFill /> : <BsFillPauseFill />}
-            
-          </button>
-          { status !== 'stopped' &&
-            <button 
-                onClick={ () => onClickStop('stopped') }
-                className="stop"
-            >
-              <BsFillSquareFill/>
-            </button>
-          }  
+          {status !== 'code' && this.renderPlayPouseButton(status, handleClick)}
+          { status !== 'stopped' && this.renderStopButton(onClickStop) }  
         </div>
     )
   }
